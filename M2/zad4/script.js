926d57ed06b7f77a74e6ce82f8881c837c59fc30
Patryk Kalinowski <p.kalinowski.93@gmail.com>
2017-10-20 21:30:06 +0200

    improvements

--- M2/zad4/script.js
+++ M2/zad4/script.js
@@ -23,14 +23,26 @@ var Counter =  React.createClass({
           React.createElement('span', {},'Licznik ' + this.state.counter),
           React.createElement('button', {onClick: this.decrement, type: 'button', className: 'btn btn-primary js-btn-minus'},'Zmniejsz'),
           React.createElement('button', {onClick: this.increment, type: 'button', className: 'btn btn-primary js-btn-plus'},'Zwiększ')
+        ),
+        React.createElement('div', {id:1, className: 'timer'},
+          React.createElement('span', {},'Licznik ' + this.state.counter),
+          React.createElement('button', {onClick: this.decrement, type: 'button', className: 'btn btn-primary js-btn-minus'},'Zmniejsz'),
+          React.createElement('button', {onClick: this.increment, type: 'button', className: 'btn btn-primary js-btn-plus'},'Zwiększ')
+        ),
+        React.createElement('div', {id:1, className: 'timer'},
+          React.createElement('span', {},'Licznik ' + this.state.counter),
+          React.createElement('button', {onClick: this.decrement, type: 'button', className: 'btn btn-primary js-btn-minus'},'Zmniejsz'),
+          React.createElement('button', {onClick: this.increment, type: 'button', className: 'btn btn-primary js-btn-plus'},'Zwiększ')
+        ),
+        React.createElement('div', {id:1, className: 'timer'},
+          React.createElement('span', {},'Licznik ' + this.state.counter),
+          React.createElement('button', {onClick: this.decrement, type: 'button', className: 'btn btn-primary js-btn-minus'},'Zmniejsz'),
+          React.createElement('button', {onClick: this.increment, type: 'button', className: 'btn btn-primary js-btn-plus'},'Zwiększ')
         )
       );
   }
 });
 
 var element = React.createElement(Counter);
-var element1 = React.createElement(Counter);
-var element2 = React.createElement(Counter);
-var element3 = React.createElement(Counter);
 
 ReactDOM.render(element, document.getElementById('app'));
