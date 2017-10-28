a261db36371dc864437f052f09eb9c87b7fc16c4
Patryk Kalinowski <p.kalinowski.93@gmail.com>
2017-10-28 13:32:51 +0200

    M2 zad3 fixed

--- M2/zad3/components/ContactForm.js
+++ M2/zad3/components/ContactForm.js
@@ -2,7 +2,28 @@ var ContactForm = React.createClass({
   propTypes: {
     contact: React.PropTypes.object.isRequired
   },
-
+  getInitialState() {
+    return {
+      firstName: '',
+      lastName: '',
+      email: ''
+    };
+  },
+  changeFirstName: function(event) {
+    this.setState({
+      firstName: event.target.value
+    });
+  },
+  changeLastName: function(event) {
+    this.setState({
+      lastName: event.target.value
+    });
+  },
+  changeEmail: function(event) {
+    this.setState({
+      email: event.target.value
+    });
+  },
   render: function() {
     return (
       React.createElement('form', {className: 'contactForm'},
@@ -10,19 +31,22 @@ var ContactForm = React.createClass({
           type: 'text',
           className: 'form-control',
           placeholder: 'Imię',
-          value: this.props.contact.firstName,
+          value: this.state.firstName,
+          onChange: this.changeFirstName
         }),
         React.createElement('input', {
           type: 'text',
           className: 'form-control',
           placeholder: 'Nazwisko',
-          value: this.props.contact.lastName,
+          value: this.state.lastName,
+          onChange: this.changeLastName
         }),
         React.createElement('input', {
           type: 'email',
           className: 'form-control',
           placeholder: 'Email',
-          value: this.props.contact.email,
+          value: this.state.email,
+          onChange: this.changeEmail
         }),
         React.createElement('button', {type: 'submit', className: 'btn btn-primary'}, "Dodaj kontakt")
       )
