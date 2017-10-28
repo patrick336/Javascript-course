d6dc32c1036d3eed1b2c5d46a86e50a020897526
Patryk Kalinowski <p.kalinowski.93@gmail.com>
2017-10-28 13:18:21 +0200

    M3 zad1 done

---
+++ M3/zad1/script-compiled.js
@@ -0,0 +1,56 @@
+"use strict";
+
+//Zadanie pierwsze
+
+var name = "John";
+var sourname = "Smith";
+
+console.log("\nZadanie pierwsze.. \n");
+console.log(name + " " + sourname);
+
+//Zadanie drugie
+console.log("\nZadanie drugie.. \n");
+var multiply = function multiply(a) {
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+  return a * b;
+};
+
+console.log(multiply(2, 5));
+console.log(multiply(2));
+
+//Zadanie trzecie
+console.log("\nZadanie trzecie.. \n");
+var average = function average() {
+  for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+    args[_key] = arguments[_key];
+  }
+
+  var result = 0;
+  args.forEach(function (arg) {
+    return result += arg;
+  });
+  return result / args.length;
+};
+
+console.log(average()); // NaN
+console.log(average(1)); // 1
+console.log(average(1, 3)); // 2
+console.log(average(1, 3, 6, 6)); // 4
+
+//Zadanie czwarte
+console.log("\nZadanie czwarte.. \n");
+
+var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+
+console.log(average.apply(undefined, grades));
+
+//Zadanie piąte
+console.log("\nZadanie piąte.. \n");
+
+var strangeArray = [1, 4, 'Iwona', false, 'Nowak'];
+var firstName = strangeArray[2],
+    lastName = strangeArray[4];
+
+
+console.log(firstName);
+console.log(lastName);
---
+++ M3/zad1/script.js
@@ -0,0 +1,42 @@
+//Zadanie pierwsze
+const name = "John";
+const sourname = "Smith";
+
+console.log("\nZadanie pierwsze.. \n");
+console.log(`${name} ${sourname}`);
+
+//Zadanie drugie
+console.log("\nZadanie drugie.. \n");
+const multiply = (a, b = 1) => { return a*b };
+
+console.log(multiply(2,5));
+console.log(multiply(2));
+
+//Zadanie trzecie
+console.log("\nZadanie trzecie.. \n");
+const average = (...args) => {
+  let result = 0;
+  args.forEach(arg => result += arg);
+  return result/args.length;
+};
+
+console.log(average()); // NaN
+console.log(average(1)); // 1
+console.log(average(1, 3)); // 2
+console.log(average(1, 3, 6, 6)); // 4
+
+//Zadanie czwarte
+console.log("\nZadanie czwarte.. \n");
+
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+
+console.log(average(...grades));
+
+//Zadanie piąte
+console.log("\nZadanie piąte.. \n");
+
+const strangeArray = [1, 4, 'Iwona', false, 'Nowak'];
+const [ , , firstName, , lastName] = strangeArray;
+
+console.log(firstName);
+console.log(lastName);
