e612b245ba9d15fba1418ba4b0ac9fdd6aa1e036
Patryk Kalinowski <p.kalinowski.93@gmail.com>
2017-11-01 18:11:09 +0100

    M3 zad 1 dla ambitnych done

--- M3/zad1/script-compiled.js
+++ M3/zad1/script-compiled.js
@@ -46,7 +46,7 @@ console.log("\nZadanie czwarte.. \n");
 
 var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
 
-//console.log(average(...grades));
+console.log(average.apply(undefined, grades));
 
 //Zadanie piąte
 console.log("\nZadanie piąte.. \n");
--- M3/zad1/script.js
+++ M3/zad1/script.js
@@ -35,7 +35,7 @@ console.log("\nZadanie czwarte.. \n");
 
 const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
 
-//console.log(average(...grades));
+console.log(average(...grades));
 
 //Zadanie piąte
 console.log("\nZadanie piąte.. \n");
