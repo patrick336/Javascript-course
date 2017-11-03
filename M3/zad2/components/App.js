f99067cd638605a27180ff919373cbd9b869eb52
Patryk Kalinowski <p.kalinowski.93@gmail.com>
2017-11-03 21:57:26 +0100

    M3 zad2 done

--- M3/zad2/components/App.js
+++ M3/zad2/components/App.js
@@ -5,42 +5,46 @@ App = React.createClass({
 
   getInitialState() {
     return {
-        loading: false,
-        searchingText: '',
-        gif: {}
+      loading: false,
+      searchingText: '',
+      gif: {}
     };
   },
   handleSearch: function(searchingText) {
     this.setState({
       loading: true
     });
-    this.getGif(searchingText, function(gif) {
+
+    this.getGif(searchingText)
+    .then(response => {
       this.setState({
         loading: false,
-        gif: gif,
+        gif: {
+          url : response.fixed_width_downsampled_url,
+          sourceUrl: response.url
+        },
         searchingText: searchingText
       });
-    }.bind(this));
+    })
+    .catch(error => console.error('Something went wrong', error));
   },
-  getGif: function(searchText, callback) {
+  getGif: function(searchText) {
     var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchText;
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.onload = function(){
-      if(xhr.status === 200) {
-        console.log("xhr.responseText" , xhr.responseText);
-        var data = JSON.parse(xhr.responseText).data;
-        console.log(data);
-        var gif = {
-          url : data.fixed_width_downsampled_url,
-          sourceUrl: data.url
-        };
-        callback(gif);
-      }
-    };
-    xhr.send()
-  },
 
+    return new Promise(
+      function (resolve, reject) {
+        const request = new XMLHttpRequest();
+        request.onload = function () {
+          this.status === 200 ? resolve(this.response) : reject(new Error(this.statusText));
+        };
+        request.onerror = function () {
+          reject(new Error(`XMLHttpRequest Error: ${this.statusText}`));
+        };
+        request.open('GET', url);
+        request.send();
+      }
+    );
+  },
   render: function () {
     var styles = {
       margin: '0 auto',
@@ -51,15 +55,15 @@ App = React.createClass({
     return (
       <div style={styles}>
         <h1> Wyszukiwarka gifów</h1>
-          <p>
-            Znajdź gifa na <a href={'http://giphy.com'} >Naciskaj ENTER, aby pobrać kolejne gify </a>
-          </p>
-          <Search onSearch={this.handleSearch} />
-          <Gif
-            loading={this.state.loading}
-            url={this.state.gif.url}
-            sourceUrl={this.state.gif.sourceUrl}
-          />
+        <p>
+          Znajdź gifa na <a href={'http://giphy.com'} >Naciskaj ENTER, aby pobrać kolejne gify </a>
+        </p>
+        <Search onSearch={this.handleSearch} />
+        <Gif
+          loading={this.state.loading}
+          url={this.state.gif.url}
+          sourceUrl={this.state.gif.sourceUrl}
+        />
       </div>
     );
   }
--- M3/zad2/components/Gif.js
+++ M3/zad2/components/Gif.js
@@ -10,13 +10,12 @@ Gif = React.createClass({
   getUrl: function () {
     return this.props.sourceUrl || GIPHY_LOADING_URL;
   },
-
   render: function () {
     var url = this.props.loading ? GIPHY_LOADING_URL : this.props.url;
 
     return (
       <div style={styles}>
-       <a href={this.getUrl()} title='view this on giphy' target='new'>
+        <a href={this.getUrl()} title='view this on giphy' target='new'>
           <img id='gif' src={ url } style={{ width: '100%', maxWidth: '350px'}} />
        </a>
       </div>
