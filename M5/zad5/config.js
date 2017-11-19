d3ff2b7b32363969166049c9b1f05c172bbf485b
Patryk Kalinowski <p.kalinowski.93@gmail.com>
2017-11-19 22:46:23 +0100

    M5 zad5 init

---
+++ M5/zad5/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '87859373867-avea8t2hrl2ur0q57ur08jv3r1itsn79.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '3fZKYsKH9BgxROccJQvwWnsu',
+    'CALLBACK_URL': 'http://localhost:8080/auth/google/callback'
+};
