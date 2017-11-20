8c3f2cb70bcad2a05eb1ce167cb803a66a4538e5
Patryk Kalinowski <p.kalinowski.93@gmail.com>
2017-11-20 22:19:39 +0100

    M5 zad5 done

--- M5/zad5/config.js
+++ M5/zad5/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'GOOGLE_CLIENT_ID': '87859373867-avea8t2hrl2ur0q57ur08jv3r1itsn79.apps.googleusercontent.com',
-    'GOOGLE_CLIENT_SECRET': '3fZKYsKH9BgxROccJQvwWnsu',
-    'CALLBACK_URL': 'http://localhost:8080/auth/google/callback'
+    'GOOGLE_CLIENT_ID': '376417467899-sa220k7l1vl5oohubq4lbse9ku1hqj1h.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'I_VyV6W5SNYL3l7QvmoMf2ew',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
 };
--- M5/zad5/server.js
+++ M5/zad5/server.js
@@ -13,17 +13,17 @@ passport.deserializeUser(function(obj, done) {
 });
 
 passport.use(new GoogleStrategy({
-        clientID: config.GOOGLE_CLIENT_ID,
-        clientSecret:config.GOOGLE_CLIENT_SECRET,
-        callbackURL: config.CALLBACK_URL
-    },
-    function(accessToken, refreshToken, profile, cb) {
-        googleProfile = {
-            id: profile.id,
-            displayName: profile.displayName
-        };
-        cb(null, profile);
-    }
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret:config.GOOGLE_CLIENT_SECRET,
+    callbackURL: config.CALLBACK_URL
+},
+function(accessToken, refreshToken, profile, cb) {
+    googleProfile = {
+        id: profile.id,
+        displayName: profile.displayName
+    };
+    cb(null, profile);
+}
 ));
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -31,7 +31,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.get('/', function (req, res) {
-    res.render('main_template', { user: req.user });
+    res.render('main_template');
 });
 
 app.get('/logged', function(req, res){
@@ -40,12 +40,12 @@ app.get('/logged', function(req, res){
 //Passport routes
 app.get('/auth/google',
 passport.authenticate('google', {
-scope : ['profile', 'email']
+    scope : ['profile', 'email']
 }));
 app.get('/auth/google/callback',
-    passport.authenticate('google', {
-        successRedirect : '/logged',
-        failureRedirect: '/'
-    }));
+passport.authenticate('google', {
+    successRedirect : '/logged',
+    failureRedirect: '/'
+}));
 
-app.listen(8080);
+app.listen(3000);
